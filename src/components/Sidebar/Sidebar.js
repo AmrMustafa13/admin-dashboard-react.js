bd055af9833f7e90b8eca892969de24589c3d698
AmrMustafa13 <amrmostafa1311@gmail.com>
2022-12-28 21:19:16 +0200

    files-structure and sidebar component

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,88 @@
+import React from "react";
+import "./Sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PeopleIcon from "@mui/icons-material/People";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import AnalyticsIcon from "@mui/icons-material/Analytics";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import HealthAndSafetyIcon from "@mui/icons-material/HealthAndSafety";
+import HistoryIcon from "@mui/icons-material/History";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">Dashboard</span>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">Main</p>
+          <li>
+            <DashboardIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
+          <p className="title">Lists</p>
+          <li>
+            <PeopleIcon className="icon" />
+            <span>Users</span>
+          </li>
+          <li>
+            <ShoppingCartIcon className="icon" />
+            <span>Products</span>
+          </li>
+          <li>
+            <CreditCardIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">Useful</p>
+          <li>
+            <AnalyticsIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">Service</p>
+          <li>
+            <HealthAndSafetyIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <HistoryIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">User</p>
+          <li>
+            <AccountCircleIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div className="color-option"></div>
+        <div className="color-option"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./home.scss";
+import Sidebar from "../../components/Sidebar/Sidebar";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="home-container">con</div>
+    </div>
+  );
+};
+
+export default Home;
