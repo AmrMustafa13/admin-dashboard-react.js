759e41951a76f8bd07b2123ab488b8bd9a4f735e
AmrMustafa13 <amrmostafa1311@gmail.com>
2023-01-05 22:40:53 +0200

    single page added

--- src/pages/Single/Single.js
+++ src/pages/Single/Single.js
@@ -1,8 +1,58 @@
 import React from "react";
 import "./single.scss";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Navbar from "../../components/Navbar/Navbar";
+import Chart from "../../components/Chart/Chart";
+import List from "../../components/Table/Table";
 
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="single-container">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="edit-btn">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                className="item-image"
+                alt=""
+                src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+              />
+              <div className="details">
+                <h1 className="item-title">John Doe</h1>
+                <div className="detail-item">
+                  <span className="item-key">Email:</span>
+                  <span className="item-key">amrmostafa@gmail.com</span>
+                </div>
+                <div className="detail-item">
+                  <span className="item-key">Phone:</span>
+                  <span className="item-key">+123456789</span>
+                </div>
+                <div className="detail-item">
+                  <span className="item-key">Address:</span>
+                  <span className="item-key">39 Mansoura</span>
+                </div>
+                <div className="detail-item">
+                  <span className="item-key">Country:</span>
+                  <span className="item-key">Egypt</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={3 / 1} title="User Spending ( Last 6 Months )" />
+          </div>
+        </div>
+        <div className="bottom">
+          <h1 className="title">Last Transactions</h1>
+          <List />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Single;
