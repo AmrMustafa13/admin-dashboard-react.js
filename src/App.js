bb231832b83e35efbbbe5718da7123435ecb2626
AmrMustafa13 <amrmostafa1311@gmail.com>
2023-01-06 15:25:12 +0200

    rename app css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import "./app.css";
+import "./App.css";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import List from "./pages/List/List";
